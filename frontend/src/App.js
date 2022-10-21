1c557d487ee191e28742e515c114bf3855f9fa65
Anil Awasthi <awasthianil73@gmail.com>
2022-10-21 18:33:13 +0545

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const App = () => {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "images-gallery"));
+};
+var _default = App;
+exports.default = _default;
